b3b9f10507f87c1d145c57b51f948f64e3dfdfab
SimonCK666 <SimonYang223@163.com>
2022-07-21 17:41:02 +0800

    'Day1_basic_ideas'

---
+++ 220721BasicPythonProgram/G10_SimonYang/DBS.R
@@ -0,0 +1,12 @@
+print("hello r")
+
+d = read.csv("E:\\NTUAILab\\220721BasicPythonProgram\\DBS_SingDollar.csv")
+
+model = lm(d$DBS ~d$SGD, data = d)
+print(model)
+
+pred = predict(model, newdata = d)
+print(pred)
+
+rmse = mean((d$DBS - pred) ^ 2) ^ 0.5
+print(rmse)
