0bc07f42aba1d87b45ce6db3473f08dea7cd7109
SimonCK666 <SimonYang223@163.com>
2022-08-04 13:49:33 +0800

    'fix_bug'

--- CervicalCancerRiskClassification/hqNet.py
+++ CervicalCancerRiskClassification/hqNet.py
@@ -13,6 +13,7 @@ from hub import load_state_dict_from_url
 from typing import Type, Any, Callable, Union, List, Optional
 from torch_deform_conv.layers import ConvOffset2D3x3
 from torch_deform_conv.layers import ConvOffset2D1x1
+from torch_deform_conv.layers import ConvOffset2D
 
 
 __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
@@ -36,7 +37,7 @@ model_urls = {
 def conv3x3(in_planes: int, out_planes: int, stride: int = 1, groups: int = 1, dilation: int = 1) -> nn.Conv2d:
     """3x3 convolution with padding"""
     return nn.Sequential(
-        ConvOffset2D3x3(in_planes),
+        ConvOffset2D(in_planes),
         nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
                      padding=dilation, groups=groups, bias=False, dilation=dilation)
     )
@@ -45,7 +46,7 @@ def conv3x3(in_planes: int, out_planes: int, stride: int = 1, groups: int = 1, d
 def conv1x1(in_planes: int, out_planes: int, stride: int = 1) -> nn.Conv2d:
     """1x1 convolution"""
     return nn.Sequential(
-        ConvOffset2D1x1(in_planes),
+        ConvOffset2D(in_planes),
         nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
     )
 
